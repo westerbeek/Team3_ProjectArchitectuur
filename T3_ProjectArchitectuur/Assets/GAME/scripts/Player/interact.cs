0c1da5ef9417346335dd3001525809ff61565488
westerbeek <roy-westerbeek@hotmail.com>
2020-06-19 16:10:13 +0200

    pushed

---
+++ T3_ProjectArchitectuur/Assets/GAME/scripts/Player/interact.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interact : MonoBehaviour
+{
+    RaycastHit hit;
+    public GameObject activatablething;
+    public GameObject inv;
+    public GameObject GameController;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameController = GameObject.Find("GameController");
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        Debug.DrawRay(transform.position, fwd * 10, Color.green);
+
+
+        if (Physics.Raycast(transform.position, fwd, out hit, 10))
+        {
+            if (hit.collider.tag == "Interactable")
+            {
+
+                if (Input.GetKeyDown("e"))
+                {
+                    Debug.Log("raycasttest");
+                    activatablething = hit.collider.gameObject;
+                    interactobj();
+
+                    Debug.Log("raycasttest2");
+                    
+                    //pickup activate = activatablething.GetComponent<pickup>();
+                    //activate.activation = true;
+                }
+            }
+        }
+    }
+
+    void interactobj()
+    {
+        if (activatablething.name == "puzzle1activator")
+        {
+            activatablething.GetComponent<puzzle1>().isplaying = true;
+            activatablething.GetComponent<puzzle1>().startpuzzle();
+            GameController.GetComponent<tasks>().task = 1;
+            if (GameController.GetComponent<pause>().gopause == false)
+            {
+                GameController.GetComponent<pause>().gopause = true;
+                activatablething = null;
+            }
+        }
+    }
+}
---
+++ T3_ProjectArchitectuur/Assets/GAME/scripts/world/menus.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menus : MonoBehaviour
+{
+    public GameObject Mainmenu;
+    public GameObject GameController;
+    public Camera Playercam;
+    public Camera mainmenucam;
+    public Transform spawn;
+    public bool enabled;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        GameController = GameObject.Find("GameController");
+        Mainmenu = GameObject.Find("MainMenu");
+        spawn = GameObject.Find("spawnlocation").transform;
+        Playercam = GameObject.Find("Playercam").GetComponent<Camera>();
+        mainmenucam = GameObject.Find("menucam").GetComponent<Camera>();
+        Playercam.enabled = false;
+        mainmenucam.enabled = true;
+        enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mainmenu.GetComponent<Canvas>().enabled == true)
+        {
+            GameController.GetComponent<pause>().gopause = true;
+             enabled = true;
+
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Playercam.enabled = false;
+            mainmenucam.enabled = true;
+            Mainmenu.GetComponent<Canvas>().enabled = true;
+            GameController.GetComponent<pause>().gopause = true;
+            Cursor.visible = true;
+
+        }
+    }
+    public void Play()
+    {
+
+        Mainmenu.GetComponent<Canvas>().enabled = false;
+        GameController.GetComponent<pause>().gopause = false;
+        Playercam.enabled = true;
+        mainmenucam.enabled = false;
+        enabled = false;
+        Cursor.visible = false;
+    }
+
+
+}
